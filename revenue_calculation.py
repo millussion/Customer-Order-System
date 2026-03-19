201843ce611ce94c64e3c7557bd8f19b892b9ac7
onomauri <onofremauro0@gmail.com>
2026-03-19 07:02:41 -0500

    v1.0

--- revenue_calculation.py
+++ revenue_calculation.py
@@ -1,3 +1,5 @@
+
+
 def calculate_total(products):
     total = 0
     for name, (price, amount, id) in products.items():
@@ -7,4 +9,6 @@ def calculate_total(products):
 my_products= products_registration()
 
 r_total= calculate_total(my_products)
-print(r_total)
+print("=================================================")
+print(f"The total revenue of the day is:{r_total}")
+print("=================================================")
