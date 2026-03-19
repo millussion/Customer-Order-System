53c10fb162d0dc49e86e37c09aef64fd73aa3b5b
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-03-19 09:34:49 -0500

    Update print.py

--- print.py
+++ print.py
@@ -1,18 +1,27 @@
-# ESTO VA EN EL MAIN
-#from first import products_registration
-#from three import print_products
-#data = products_registration()
-#print_products(data)
+def view_orders(orders, clients, products): #receives 3 dicts
 
+    if not orders:
+        return "No orders registered." #if there are no orders return message
 
-def print_products(products):
-    print("\n=== PRODUCT LIST ===")
+    result = "" #here all the info will be stored to display
 
-    # Ahora 'key' es el ID y 'value' es la tupla (name, price, amount)
-    for prod_id, info in products.items():
-        name, price, amount = info
-        print(f"ID: {prod_id}")
-        print(f"Name: {name}")
-        print(f"Price: {price}")
-        print(f"amount: {amount}")
-        print("-------------------")
+    for order_id, order_data in orders.items(): #loops through each order, .items() gets both id and data
+        #order_id stores the id and order_data stores the values of orders
+
+        client_id = order_data[0] #0 because client_id is at position 0 in the tuple
+        product_id = order_data[1]
+        quantity = order_data[2]
+        total = order_data[3]
+
+        client_name = clients[client_id]["name"]
+        product_name = products[product_id][0]
+    #format the information to print
+        result += f"""
+Order ID: {order_id}
+Client: {client_name}
+Product: {product_name}
+Quantity: {quantity}
+Total: {total}
+"""
+
+    return result
