6c5380d8664fc2aa608527b45bf227623eb26e55
Moises Alfredo Gutiérrez Haad <ghmoises97@gmail.com>
2026-03-19 08:31:23 -0500

    correction

--- products_registration.py
+++ products_registration.py
@@ -1,37 +1,29 @@
-products = {}
-
-def products_registration():
+def products_registration(products):
     valid = False
-    
     while valid == False: 
         try: 
-            count = int(input("Please enter the number of products you wnat to add: "))
+            count = int(input("Please enter the number of products you want to add: "))
             valid = True
         except ValueError:
             print ("Invalid input. Please enter a number.")
     for i in range (count):
+        print(f"Product", i+1)
         name = input ("Enter the name of the product: ")
-        
+
         valid = False
         while valid == False: 
+
             try:
                 price = float (input("Enter the price of the product: "))
                 valid = True
             except ValueError:
                 print("Invalid inpud. Please enter a number.")
-        
+
+
         valid = False
         while valid == False:
             try: 
-                amount = int (input("enter the amount of the product: "))
-                valid = True
-            except ValueError: 
-                print ("Invalid input. PLease enter a number.")
-        
-        valid = False
-        while valid == False:
-            try: 
-               id_input = int (input ("Enter the ID of the products; "))
+               id_input = int (input ("Enter the ID of the products: "))
                if id_input in products:
                    print ("Error: this ID already exists. Please enter a different ID.")
                else: 
@@ -39,7 +31,8 @@ def products_registration():
                    valid =True
             except ValueError: 
                 print ("Invalid input. Please enter a number")
-                
-                
-        products [id] = (name, price, amount) 
+
+
+        products [id] = (name, price) 
+    print("Products saved successfully")
     return products
